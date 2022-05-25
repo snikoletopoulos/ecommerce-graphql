56ac918cd285c325a771f8ce2c72f3fb9e74fc08
Stavros <snikoletopoulos@hotmail.com>
2022-05-25 23:03:20 +0300

    Create a resolver for Query and filter the products

---
+++ src/resolvers/Query.js
@@ -0,0 +1,36 @@
+exports.Query = {
+	hello: () => "Hello world!",
+	products: (_, { filter }, { products, reviews }) => {
+		if (!filter) return products;
+
+		let filteredProducts = products;
+
+		if ("onSale" in filter) {
+			filteredProducts = filteredProducts.filter(
+				product => product.onSale === filter.onSale
+			);
+		}
+
+		if ([1, 2, 3, 4, 5].includes(filter?.avgRating)) {
+			filteredProducts = filteredProducts.filter(product => {
+				const productReviews = reviews.filter(
+					review => review.productId === product.id
+				);
+
+				const avgRating =
+					productReviews.reduce((acc, review) => {
+						return acc + review.rating;
+					}, 0) / productReviews.length;
+
+				return avgRating >= filter.avgRating;
+			});
+		}
+
+		return filteredProducts;
+	},
+	product: (_, { id }, { products }) =>
+		products.find(product => product.id === id),
+	categories: () => categories,
+	category: (_, { id }, { categories }) =>
+		categories.find(category => category.id === id),
+};
